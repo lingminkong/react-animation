809e2f6e426ebd4b3997484fcb24dff733becffb
Michael Kong <michael@vivodigital.com.au>
2020-06-04 17:14:28 +1000

    Added Banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
+      <Banner />
     </div>
   );
 }
