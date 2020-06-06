168234e06a94cdbe4b69f2ef893412c28e950d68
Michael Kong <michael@vivodigital.com.au>
2020-06-07 00:07:49 +1000

    Finished react gsap animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Cases from './components/Cases';
+import IntroOverlay from './components/introOverlay';
+import gsap from 'gsap';
 
 function App() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    gsap.to('body', 0, { css: { visibility: 'visible' } });
+
+    const tl = gsap.timeline();
+    tl.from('.line span', 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.5,
+      },
+    })
+      .to('.overlay-top', 1.6, {
+        height: 0,
+        ease: 'expo.inOut',
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to('.overlay-bottom', 1.6, {
+        width: 0,
+        ease: 'expo.inOut',
+        delay: -0.8,
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .from('.case-image img', 1.6, {
+        scale: 1.4,
+        delay: -2,
+        ease: 'expo.inOut',
+        stagger: {
+          amount: 0.4,
+        },
+      });
   }, []);
   return (
-    <div className="App">
+    <div className='App'>
+      <IntroOverlay />
       <Header />
       <Banner />
       <Cases />
